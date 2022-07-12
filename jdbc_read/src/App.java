af150c8de5e8a3dbe68008b3a3af00ab957697f3
Lucas Bernardes <lucasbernardes24@gmail.com>
2022-07-12 13:51:35 -0300

    read from a database

---
+++ jdbc_read/src/App.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try{
+            con = DB.getConnection();
+            st = con.createStatement();
+            rs = st.executeQuery("select * from department");
+
+            while(rs.next()){
+                System.out.println(rs.getInt("Id") + " | " + rs.getString("Name"));
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        finally{
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
